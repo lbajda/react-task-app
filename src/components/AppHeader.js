e3b93d119828bc6e6c46018215868dac6d4a41b5
lbajda <bajda.luke@gmail.com>
2023-09-28 09:59:40 -0500

    update AppHeader.js

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Button, { SelectButton } from './Button'
-import styles from '../styles/modules/app.module.scss'
 import TodoModal from './TodoModal'
 import { updateFilterStatus } from '../slices/todoSlice'
 
@@ -23,7 +22,6 @@ export default function AppHeader() {
       </Button>
       <SelectButton
         id='status'
-        // className='block w-40 mt-1 bg-gray-200  border px-4 py-2 outline-none rounded-md shadow-sm focus:border-gray-300  focus:ring-4 focus:ring-gray-200 focus:outline-none focus:ring-opacity-75 custom-select'
         className='w-40 px-5 py-2.5 inline-flex justify-center items-center gap-2 rounded-lg bg-gray-100 border border-transparent font-medium text-gray-500 hover:text-white hover:bg-gray-500 focus:outline-none focus:ring-2 ring-offset-white focus:ring-gray-500 focus:ring-offset-2 transition-all duration-300 text-md dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-600 dark:text-white dark:focus:ring-offset-gray-800 custom-select dark:custom-select'
         onChange={(e) => updateFilter(e)}
         value={filterStatus}>
